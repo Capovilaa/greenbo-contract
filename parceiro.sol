bb401d07bd49733d6ee52f701eee443a636f09df
Capovila Lucas (CaP/ETS) <lcapovila@outlook.com>
2023-10-30 13:54:11 -0300

    Contrato consegue transferir todo seu saldo para outra carteria ou contrato

--- parceiro.sol
+++ parceiro.sol
@@ -2,11 +2,12 @@
 pragma solidity 0.8.18;
 
 contract Parceiro {
-    // Ideia inicial é de criar o contato com campos dos dados e realizar a leitura depois com funções    
+    // Ideia inicial é de criar o contato com campos dos dados e realizar a leitura depois com funções
     address idParceiro;
     address immutable carteira;
     address[] private senders; // Quem manda
     address[] private funders; // Quem recebe
+    address private projeto_um = 0x95E7851B6a390dA02c6552D4dd28af202e5630BF; // Address do contrato que recebe os créditos de carbono
 
     int256 private saldoCreditoCarbono;
 
@@ -14,119 +15,141 @@ contract Parceiro {
     uint256 fator = 2;
 
     // Para mapear históricos das leituras e transações
-    mapping (uint256 => uint256) private historicoConsumoEnergia;
-    mapping (uint256 => uint256) private historicoConsumoCO2;   
-    mapping (uint256 => int256) private historicoSalcoCC;   
-    mapping (address => uint256) private historicoTransacoes;
-    mapping (uint256 => bytes32) private historicoHash; // ?
-    mapping (uint256 => uint256) private historicoPlanejamento;
+    mapping(uint256 => uint256) private historicoConsumoEnergia;
+    mapping(uint256 => uint256) private historicoConsumoCO2;
+    mapping(uint256 => int256) private historicoSalcoCC;
+    mapping(address => uint256) private historicoTransacoes;
+    mapping(uint256 => bytes32) private historicoHash; // ?
+    mapping(uint256 => uint256) private historicoPlanejamento;
 
-    // Struct com as informações necessárias para serem armazenadas  
-    struct Transacoes {   
-        uint256 idGravado;     
+    // Struct com as informações necessárias para serem armazenadas
+    struct Transacoes {
+        uint256 idGravado;
         uint256 consumoCO2;
-        uint256 planejamentoConsumoMes;        
+        uint256 planejamentoConsumoMes;
         uint256 consumoEnergia;
         uint256 mes;
     }
 
-    Transacoes[] private transacoes;    
+    Transacoes[] private transacoes;
 
     // Construtor para salvar a carteira
-    constructor(){
+    constructor() {
         carteira = msg.sender;
     }
 
     // Função que vai gravar os dados do parceiro na Blockchain
-    function gravarDados(uint256 _consumoCO2, uint256 _planejamentoConsumoMes, uint256 _consumoEnergia, uint256 _mes) public {
-        
+    function gravarDados(
+        uint256 _consumoCO2,
+        uint256 _planejamentoConsumoMes,
+        uint256 _consumoEnergia,
+        uint256 _mes
+    ) public {
         // Cria um objeto para armazenar as informações vindas por parâmetro
-        Transacoes memory novaTransacao = Transacoes(idGravacao, _consumoCO2, _planejamentoConsumoMes, _consumoEnergia, _mes);
+        Transacoes memory novaTransacao = Transacoes(
+            idGravacao,
+            _consumoCO2,
+            _planejamentoConsumoMes,
+            _consumoEnergia,
+            _mes
+        );
         transacoes.push(novaTransacao);
 
         // Armazena nos mappings para acessos futuros
         historicoConsumoEnergia[idGravacao] = _consumoEnergia;
-        historicoConsumoCO2[idGravacao] = _consumoEnergia;  
+        historicoConsumoCO2[idGravacao] = _consumoEnergia;
         historicoPlanejamento[idGravacao] = _planejamentoConsumoMes;
 
         // Após cada leitura, atualizar o saldo do crédito de carbono
-        alteraCreditoCarbono(_consumoEnergia, _planejamentoConsumoMes);    
+        alteraCreditoCarbono(_consumoEnergia, _planejamentoConsumoMes);
     }
 
     // Função para calcular crédito de carbono dele (planejamento - emissão) * fator
-    function alteraCreditoCarbono (uint256 _consumo, uint256 _previsao) private  {
-        int256 calculo = (int256(_previsao) - int256(_consumo)) * int256(fator);                
-        saldoCreditoCarbono += calculo;        
+    function alteraCreditoCarbono(uint256 _consumo, uint256 _previsao) private {
+        int256 calculo = (int256(_previsao) - int256(_consumo)) * int256(fator);
+        saldoCreditoCarbono += calculo;
         historicoSalcoCC[idGravacao] = saldoCreditoCarbono;
 
         // idGravacao é a sentinela que controla o índice
         idGravacao++;
     }
-    
 
     // Função para adicionar fundos ao contrato (créditos de carbono)
     function adicionaFundos() public payable {
         funders.push(msg.sender);
         historicoTransacoes[msg.sender] = msg.value;
     }
-    
+
     receive() external payable {
         adicionaFundos();
+    }
+
+    // Função que realiza transferência
+    function transferirSaldo() public restricted{
+        payable(projeto_um).transfer(address(this).balance);
     }
 
     // ------- GETS ------- //
 
     // Função para ler os dados pelo índice
-    function getLeiturasDados(uint i) public view returns (uint256, uint256) {
+    function getLeiturasDados(uint256 i)
+        public
+        view
+        returns (uint256, uint256)
+    {
         require(i < transacoes.length, "Indice invalido");
         Transacoes memory novaTransacao = transacoes[i];
         return (novaTransacao.consumoCO2, novaTransacao.planejamentoConsumoMes);
     }
-    
+
     // Função para retornar o saldo do crédito de carbono atual
-    function getSaldoCC() public view returns(int256){
+    function getSaldoCC() public view returns (int256) {
         return saldoCreditoCarbono;
     }
 
-    // Função para retornar histórico de saldos de crédito de carbono por parâmetro    
-    function getHistSaldoCC() public  view returns (int256[] memory){
+    // Função para retornar histórico de saldos de crédito de carbono por parâmetro
+    function getHistSaldoCC() public view returns (int256[] memory) {
         int256[] memory retorno = new int256[](idGravacao);
-        for(uint i = 0; i < idGravacao; i++ ){
+        for (uint256 i = 0; i < idGravacao; i++) {
             retorno[i] = historicoSalcoCC[i];
         }
         return retorno;
-    }   
-
+    }
 
     // Função para retornar histórico de consumo de CO2 por parâmetro
-    function getHistConsumoCO2() public view returns(uint256[] memory){
+    function getHistConsumoCO2() public view returns (uint256[] memory) {
         uint256[] memory retorno = new uint256[](idGravacao);
-        for(uint i = 0; i < idGravacao; i++ ){
+        for (uint256 i = 0; i < idGravacao; i++) {
             retorno[i] = historicoConsumoCO2[i];
         }
         return retorno;
     }
 
     // Função para retornar histórico de consumo de energia por parâmetro
-    function getHistConsumoEnergia() public view returns(uint256[] memory){
+    function getHistConsumoEnergia() public view returns (uint256[] memory) {
         uint256[] memory retorno = new uint256[](idGravacao);
-        for(uint i = 0; i < idGravacao; i++ ){
+        for (uint256 i = 0; i < idGravacao; i++) {
             retorno[i] = historicoConsumoEnergia[i];
         }
         return retorno;
     }
 
     // Função para retornar histórico do planejamento de consumo
-    function getHistPlanejamento() public view returns (uint256[] memory){
+    function getHistPlanejamento() public view returns (uint256[] memory) {
         uint256[] memory retorno = new uint256[](idGravacao);
-        for(uint i = 0; i < idGravacao; i++ ){
+        for (uint256 i = 0; i < idGravacao; i++) {
             retorno[i] = historicoPlanejamento[i];
         }
         return retorno;
     }
 
     // Função para retornar o saldo do contrato
-    function getSaldo() public view returns (uint){
+    function getSaldo() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    modifier restricted() {
+        require(msg.sender == carteira);
+        _;
     }
 }
