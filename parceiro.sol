947084eed7a4ca206a521cf9430cf2d6592b09db
Capovila Lucas (CaP/ETS) <Lucas.Capovila@br.bosch.com>
2023-11-23 11:26:19 -0300

    Adicionando função de cálculo correta

--- parceiro.sol
+++ parceiro.sol
@@ -8,26 +8,44 @@ contract Parceiro {
     address[] private senders; // Quem manda
     address[] private funders; // Quem recebe
 
-    int256 private saldoCreditoCarbono = 5;
+    int256 private saldoCreditoCarbono = 0;
 
     uint256 idGravacao = 0;
-    uint256 fator = 2;
+    uint256 immutable i_fator = 426;
 
     // Para mapear históricos das leituras e transações
+    mapping(uint256 => uint256) private historicoPlanCombMovel; // planejamento combustão móvel
+    mapping(uint256 => uint256) private historicoCombMovel; // combustão móvel
+
+    mapping(uint256 => uint256) private historicoPlanCombEstac; // planejamento combustão estacionária
+    mapping(uint256 => uint256) private historicoCombEstac; // combustão estacionária
+
+    mapping(uint256 => uint256) private historicoPlanejamentoEnergia;
     mapping(uint256 => uint256) private historicoConsumoEnergia;
-    mapping(uint256 => uint256) private historicoConsumoCO2;
-    mapping(uint256 => int256) private historicoSalcoCC;
-    mapping(address => uint256) private historicoTransacoes;
-    mapping(uint256 => bytes32) private historicoHash; // ?
+
     mapping(uint256 => uint256) private historicoPlanejamentoC02;
-    mapping(uint256 => uint256) private historicoPlanejamentoEnergia; // add esse ainda
+    mapping(uint256 => uint256) private historicoConsumoCO2;
+
+
+    mapping(uint256 => int256) private historicoSalcoCC;
+
+    mapping(address => uint256) private historicoTransacoes;
+
+    mapping(uint256 => bytes32) private historicoHash;
 
     // Struct com as informações necessárias para serem armazenadas
     struct Transacoes {
         uint256 idGravado;
-        uint256 consumoCO2;
-        uint256 planejamentoConsumoMes;
+
+        uint256 planCombMovel;
+        uint256 combMovel;
+
+        uint256 planCombEstac;
+        uint256 combEstac;
+
+        uint256 planConsumoEnergia;
         uint256 consumoEnergia;
+
         uint256 mes;
     }
 
@@ -40,34 +58,57 @@ contract Parceiro {
 
     // Função que vai gravar os dados do parceiro na Blockchain
     function gravarDados(
-        uint256 _consumoCO2,
-        uint256 _planejamentoConsumoMes,
+        uint256 _planCombMovel,
+        uint256 _combMovel,
+        uint256 _planCombEstac,
+        uint256 _combEstac,
+        uint256 _planConsEnergia,
         uint256 _consumoEnergia,
         uint256 _mes
     ) public {
         // Cria um objeto para armazenar as informações vindas por parâmetro
         Transacoes memory novaTransacao = Transacoes(
             idGravacao,
-            _consumoCO2,
-            _planejamentoConsumoMes,
+            _planCombMovel,
+            _combMovel,
+            _planCombEstac,
+            _combEstac,
+            _planConsEnergia,
             _consumoEnergia,
             _mes
         );
         transacoes.push(novaTransacao);
 
         // Armazena nos mappings para acessos futuros
+        historicoPlanCombMovel[idGravacao] = _planCombMovel;
+        historicoCombMovel[idGravacao] = _combMovel;
+
+        historicoPlanCombEstac[idGravacao] = _planCombEstac;
+        historicoCombEstac[idGravacao] = _combEstac;        
+
+        historicoPlanejamentoEnergia[idGravacao] = _planConsEnergia;
         historicoConsumoEnergia[idGravacao] = _consumoEnergia;
-        historicoConsumoCO2[idGravacao] = _consumoEnergia;
-        historicoPlanejamentoC02[idGravacao] = _planejamentoConsumoMes;
+
+        uint256 teste = calcularCO2Energia(_consumoEnergia);
+
+        historicoConsumoCO2[idGravacao] = _combEstac + _combMovel + teste;
 
         // Após cada leitura, atualizar o saldo do crédito de carbono
-        alteraCreditoCarbono(_consumoEnergia, _planejamentoConsumoMes);
+        // alteraCreditoCarbono(_consumoEnergia, _planConsEnergia);        
+        alteraCreditoCarbono();        
     }
 
-    // Função para calcular crédito de carbono dele (planejamento - emissão) * fator
-    function alteraCreditoCarbono(uint256 _consumo, uint256 _previsao) private {
-        int256 calculo = (int256(_previsao) - int256(_consumo)) * int256(fator);
-        saldoCreditoCarbono += calculo;
+    // Função para calcular co2 pelo consumo de energia
+    function calcularCO2Energia(uint256 _consumoEnergia)pure private returns (uint256){
+        return _consumoEnergia * i_fator;        
+    }
+
+    // Função para calcular crédito de carbono dele (planejamento - emissão) * i_fator
+    function alteraCreditoCarbono() private {
+        if(idGravacao != 0){
+            int256 calculo = (int(historicoConsumoCO2[idGravacao-1]) - int256(historicoConsumoCO2[idGravacao]));
+            saldoCreditoCarbono += calculo;
+        }
         historicoSalcoCC[idGravacao] = saldoCreditoCarbono;
 
         // idGravacao é a sentinela que controla o índice
@@ -96,20 +137,20 @@ contract Parceiro {
     // ------- GETS ------- //
 
     // Função para ler os dados pelo índice
-    function getLeiturasDados(uint256 i)
-        public
-        view
-        returns (uint256, uint256)
-    {
-        require(i < transacoes.length, "Indice invalido");
-        Transacoes memory novaTransacao = transacoes[i];
-        return (novaTransacao.consumoCO2, novaTransacao.planejamentoConsumoMes);
-    }
+    // function getLeiturasDados(uint256 i)
+    //     public
+    //     view
+    //     returns (uint256, uint256)
+    // {
+    //     require(i < transacoes.length, "Indice invalido");
+    //     Transacoes memory novaTransacao = transacoes[i];
+    //     return (novaTransacao.consumoCO2, novaTransacao.planejamentoConsumoMes);
+    // }
 
-    // Função para retornar o saldo do crédito de carbono atual
-    function getSaldoCC() public view returns (int256) {
-        return saldoCreditoCarbono;
-    }
+    // // Função para retornar o saldo do crédito de carbono atual
+    // function getSaldoCC() public view returns (int256) {
+    //     return saldoCreditoCarbono;
+    // }
 
     // Função para retornar histórico de saldos de crédito de carbono por parâmetro
     function getHistSaldoCC() public view returns (int256[] memory) {
